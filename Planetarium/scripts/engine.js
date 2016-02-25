3d79264cfab181920f1279a0c5753d5b0a248237
noob2 <viktor.jordanov.1992@gmail.com>
2016-02-25 13:58:11 +0200

    You can now accelerate the new formed particle

--- Planetarium/scripts/engine.js
+++ Planetarium/scripts/engine.js
@@ -5,6 +5,15 @@ const WIDTH = 800;
 const HEIGHT = 800;
 
 var planetArray = [];
+//todo : tova trqbva da se mahne ottuk
+var mouseDown = 0;
+document.body.onmousedown = function() {
+    mouseDown = 1;
+}
+document.body.onmouseup = function() {
+    mouseDown = 0;
+}
+
 function init() {
 
     canvas = document.getElementById("myCanvas");
@@ -12,38 +21,61 @@ function init() {
     canvas.setAttribute('width', WIDTH);
     canvas.setAttribute('height', HEIGHT);
     canvas.style.backgroundImage= "url('images/andromeda.jpg')";
-    canvas.addEventListener("click", addPlanet, false);
+    canvas.addEventListener("mousedown", addPlanet, false);
 
-
+    var x = 0;
+    var y = 0;
+    var x2 = 0;
+    var y2 = 0;
 
     function addPlanet(event) {
-        var x = event.clientX - canvas.offsetLeft;
-        var y = event.clientY - canvas.offsetTop;
+        if (x === 0 || y === 0) {
+            x = event.clientX - canvas.offsetLeft;
+            y = event.clientY - canvas.offsetTop;
+        } else if (x !== 0 || y !== 0) {
+            x2 = event.clientX - canvas.offsetLeft;
+            y2 = event.clientY - canvas.offsetTop;
 
-        var rad = document.getElementById("radius").value;
-        var mas = document.getElementById("mass").value;
-
-        var radiusParagraph = document.getElementById("numericRadius");
-        var massParagraph = document.getElementById("numericMass");
-
-        if(rad > 0 && mas > 0){
-            planetArray.push(new Planet(x, y, 0, 0, rad, mas * 10));
+            var rad = document.getElementById("radius").value;
+            var mas = document.getElementById("mass").value;
 
 
 
-            radiusParagraph.innerHTML = rad + ' thousand kilometer';
-            massParagraph.innerHTML = mas + '0 000 000 000 000 000 000 tons';
-        } else {
-            radiusParagraph.innerHTML = 'enter number';
-            massParagraph.innerHTML = 'enter number';
+            var radiusParagraph = document.getElementById("numericRadius");
+            var massParagraph = document.getElementById("numericMass");
+
+            if(rad > 0 && mas > 0){
+                planetArray.push(new Planet(x,y,(x2-x)/100,(y2-y)/100, rad, mas * 10));
+
+
+
+
+                radiusParagraph.innerHTML = rad + ' thousand kilometer';
+                massParagraph.innerHTML = mas + '0 000 000 000 000 000 000 tons';
+            } else {
+                radiusParagraph.innerHTML = 'enter number';
+                massParagraph.innerHTML = 'enter number';
+            }
+            x = 0;
+            y = 0;
+            x2 = 0;
+            y2 = 0;
+
         }
+
+
+
+
+
+
 
     }
 
+
+
     return setInterval(draw, 10);
 }
-
-function draw() {
+function draw(e) {
     clear();
 
     for (var i = 0; i < planetArray.length; i++) {
@@ -65,4 +97,3 @@ function clear() {
 
 init();
 
-//distance = Math.sqrt(Math.abs(planet.x1)*Math.abs(planet.x1) + Math.abs(planet.y1)*Math.abs(planet.y1));
